ae711203379cd61877580e8a6cb1a14ee6348880
kubilayculha <kubilay__culha@hotmail.com>
2023-03-17 15:39:05 +0300

    Soft Assert - Hard Assert

---
+++ src/Gun06/_01_SoftAssertVsHardAssert.java
@@ -0,0 +1,35 @@
+package Gun06;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class _01_SoftAssertVsHardAssert {
+
+    @Test
+    void hardAssert() {
+     String s1="Merhaba";
+
+        System.out.println("Hard assert öncesi");
+        Assert.assertEquals("Merhaba", s1, "Oluşan la bekleme eşit değil."); // test kırılıyor burada
+        System.out.println("Hard assert sonrası");
+    }
+    @Test
+    void softAssert() {
+        String strHomePage="www.facebook.com/homepage";
+        String strCartPage="www.facebook.com/cartpage";
+        String strEditAccount="www.facebook.com/editaccountpage";
+
+        SoftAssert _softAssert = new SoftAssert();
+        _softAssert.assertEquals("www.facebook.com/homepage", strHomePage); // true
+        System.out.println("soft Assert 1");
+        _softAssert.assertEquals("www.facebook.com/profile", strCartPage); // false
+        System.out.println("soft Assert 2");
+        _softAssert.assertEquals("www.facebook.com/settings", strEditAccount); // false
+        System.out.println("soft Assert 3");
+
+        _softAssert.assertAll(); // bütün assert sonuçlarını işleme koyuyor.
+        System.out.println("soft AssertAll : aktiflik sonrası");
+        // bu bölüm assertAlldan sonra olduğu ve öncesinde hata oluştuğu için ekrana çıkmayacak
+    }
+}
